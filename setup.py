fada21fe489539e14c205e2a37dbfdec8085c15e
Gabriel Monnerat <gabriel@tiolive.com>
2011-01-13 12:31:59 +0000

    release 1.0.10
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk/utils@42301 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os import path
 import sys
 
-version = '1.0.9'
+version = '1.0.10'
 
 folder_path = path.abspath(path.dirname(__file__)) + "/cloudooo"
 
