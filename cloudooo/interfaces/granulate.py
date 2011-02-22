ce5808bb0c567e0ec905530ab04e66cf6143c5fc
Mario Jorge Amaral <mariojorgevalle@gmail.com>
2011-02-22 22:00:43 +0000

    Add parameters in the functions.
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk/utils@43584 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- cloudooo/interfaces/granulate.py
+++ cloudooo/interfaces/granulate.py
@@ -32,44 +32,44 @@ from zope.interface import Interface
 class ITableGranulator(Interface):
   """Provides methods to granulate a document into tables."""
 
-  def getTableItemList():
+  def getTableItemList(data, source_format):
     """Returns the list of table IDs in the form of (id, title)."""
 
-  def getTableItem(id, format):
+  def getTableItem(data, format, table_id, source_format):
     """Returns the table into a new 'format' file."""
 
-  def getTableMatrix(self, id):
+  def getTableMatrix(self, table_id, source_format):
     """Returns the table as a matrix."""
 
-  def getColumnItemList(file, table_id):
+  def getColumnItemList(data, table_id, source_format):
     """Return the list of columns in the form of (id, title)."""
 
-  def getLineItemList(file, table_id):
+  def getLineItemList(data, table_id, source_format):
     """Returns the lines of a given table as (key, value) pairs."""
 
 
 class IImageGranulator(Interface):
   """Provides methods to granulate a document into images."""
 
-  def getImageItemList():
+  def getImageItemList(data, source_format):
     """Return the list of images in the form of (id, title)."""
 
-  def getImage(id, format=None, resolution=None, **kw):
+  def getImage(data, image_id, source_format, format=None, resolution=None, **kw):
     """Return the given image."""
 
 
 class ITextGranulator(Interface):
   """Provides methods to granulate a document into chapters and paragraphs."""
 
-  def getParagraphItemList():
+  def getParagraphItemList(data, source_format):
     """Returns the list of paragraphs in the form of (id, class) where class
     may have special meaning to define TOC/TOI."""
 
-  def getParagraphItem(paragraph_id):
+  def getParagraphItem(data, paragraph_id, source_format):
     """Returns the paragraph in the form of (text, class)."""
 
-  def getChapterItemList(file):
+  def getChapterItemList(data, source_format):
     """Returns the list of chapters in the form of (id, level)."""
 
-  def getChapterItem(file, chapter_id):
+  def getChapterItem(data, chapter_id, source_format):
     """Return the chapter in the form of (title, level)."""
