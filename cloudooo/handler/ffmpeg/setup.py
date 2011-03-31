4fd17dd9dcd5a8311bae8c225d00ea7bcf390318
Gabriel Monnerat <gabriel@tiolive.com>
2011-03-31 20:24:14 +0000

    indent the code following pep8
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk/utils@44943 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- cloudooo/handler/ffmpeg/setup.py
+++ cloudooo/handler/ffmpeg/setup.py
@@ -3,39 +3,36 @@ from setuptools import setup, find_packages
 name = "cloudooo.handler.ffmpeg"
 version = '0.1'
 
+
 def read(name):
   return open(name).read()
 
-long_description=(read('README.txt')
-                  + '\n' +
-                  read('CHANGES.txt')
-                 )
+long_description = (read('README.txt') + '\n' + read('CHANGES.txt'))
 
 install_requires = ["zope.interface",
-                    "cloudooo",]
+                    "cloudooo"]
 
 setup(
-    name = name,
-    version = version,
-    author = "Gabriel M. Monnerat",
-    author_email = "gabriel@tiolive.com",
-    description = "Python Package to handle Videos",
-    long_description=long_description,
-    license = "GPLv3",
-    keywords = "python ffmpeg",
-    classifiers=[
-      "Programming Language :: Python :: 2.6",
-      "Natural Language :: English",
-      "Topic :: Software Development :: Libraries :: Python Modules",
-      "License :: OSI Approved :: GNU General Public License (GPL)",
-    ],
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    include_package_data = True,
-    namespace_packages = ["cloudooo", "cloudooo.handler"],
-    install_requires=install_requires,
-    entry_points="""
-    [console_scripts]
-    runFFMPEGHandlerUnitTest = cloudooo.handler.ffmpeg.tests.runFFMPEGHandlerUnitTest:run
-    """,
-    )
+  name = name,
+  version = version,
+  author = "Gabriel M. Monnerat",
+  author_email = "gabriel@tiolive.com",
+  description = "Python Package to handle Videos",
+  long_description=long_description,
+  license = "GPLv3",
+  keywords = "python ffmpeg",
+  classifiers=[
+    "Programming Language :: Python :: 2.6",
+    "Natural Language :: English",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "License :: OSI Approved :: GNU General Public License (GPL)",
+  ],
+  packages = find_packages('src'),
+  package_dir = {'': 'src'},
+  include_package_data = True,
+  namespace_packages = ["cloudooo", "cloudooo.handler"],
+  install_requires=install_requires,
+  entry_points="""
+  [console_scripts]
+  runFFMPEGHandlerUnitTest = cloudooo.handler.ffmpeg.tests.runFFMPEGHandlerUnitTest:run
+  """,)
--- cloudooo/handler/ooo/setup.py
+++ cloudooo/handler/ooo/setup.py
@@ -4,13 +4,11 @@ from setuptools import setup, find_packages
 name = "cloudooo.handler.ooo"
 version = '0.1'
 
+
 def read(name):
   return open(name).read()
 
-long_description=(read('README.txt')
-                  + '\n' +
-                  read('CHANGES.txt')
-                 )
+long_description = (read('README.txt') + '\n' + read('CHANGES.txt'))
 
 install_requires = ["zope.interface",
                     "psutil>=0.2.0",
@@ -21,27 +19,25 @@ if sys.version_info < (2, 5):
   install_requires.append('simplejson')
 
 setup(
-    name = name,
-    version = version,
-    author = "Gabriel M. Monnerat",
-    author_email = "gabriel@tiolive.com",
-    description = "Python Package to handler OpenOffice.org Documents",
-    long_description=long_description,
-    license = "GPLv3",
-    keywords = "Python OpenOffice.org",
-    classifiers= [
-      "Programming Language :: Python :: 2.6",
-      "Natural Language :: English",
-      "Topic :: Software Development :: Libraries :: Python Modules",
-      "License :: OSI Approved :: GNU General Public License (GPL)",
-
-    ],
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    namespace_packages = ["cloudooo", "cloudooo.handler"],
-    install_requires=install_requires,
-    entry_points="""
-    [console_scripts]
-    runOOoHandlerUnitTest = cloudooo.handler.ooo.tests.runOOoHandlerUnitTest:run
-    """
-    )
+  name = name,
+  version = version,
+  author = "Gabriel M. Monnerat",
+  author_email = "gabriel@tiolive.com",
+  description = "Python Package to handler OpenOffice.org Documents",
+  long_description=long_description,
+  license = "GPLv3",
+  keywords = "Python OpenOffice.org",
+  classifiers= [
+    "Programming Language :: Python :: 2.6",
+    "Natural Language :: English",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "License :: OSI Approved :: GNU General Public License (GPL)",
+  ],
+  packages = find_packages('src'),
+  package_dir = {'': 'src'},
+  namespace_packages = ["cloudooo", "cloudooo.handler"],
+  install_requires=install_requires,
+  entry_points="""
+  [console_scripts]
+  runOOoHandlerUnitTest = cloudooo.handler.ooo.tests.runOOoHandlerUnitTest:run
+  """)
