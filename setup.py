bc797752fab7565833f38b2b76a4e94facafdcbf
Nicolas Delaby <nicolas@nexedi.com>
2011-04-01 13:26:27 +0000

    release 1.2.3
    git-svn-id: https://svn.erp5.org/repos/public/erp5/trunk/utils@44991 20353a03-c40f-0410-a6d1-a30d3c3de9de

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os import path
 import sys
 
-version = '1.2.2'
+version = '1.2.3'
 
 folder_path = path.abspath(path.dirname(__file__)) + "/cloudooo"
 
