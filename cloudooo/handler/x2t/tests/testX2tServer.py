7b6a8e214b2c5e7d8d2086a15de875fe3c66510a
Tristan Cavelier <tristan.cavelier@nexedi.com>
2016-09-26 12:09:29 +0200

    x2t: add server test

---
+++ cloudooo/handler/x2t/tests/testX2tServer.py
@@ -0,0 +1,53 @@
+##############################################################################
+#
+# Copyright (c) 2016 Nexedi SA and Contributors. All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+from os.path import join
+from cloudooo.tests.cloudoooTestCase import TestCase, make_suite
+
+
+class TestServer(TestCase):
+  """Test XmlRpc Server. Needs cloudooo server started"""
+
+  def ConversionScenarioList(self):
+    return [
+      (join('data', 'test.xlsx'), "xlsx", "xlsy", "text/plain"),  # XXX no mime mapping for xlsy ?
+      (join('data', 'test.xlsy'), "xlsy", "xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"),
+    ]
+
+  def testConvertHtmltoPdf(self):
+    """Converts xlsx to xlsy and xlsy to xlsx"""
+    self.runConversionList(self.ConversionScenarioList())
+
+  def FaultConversionScenarioList(self):
+    return [
+      # Test to verify if server fail when a empty string is sent
+      ('', '', ''),
+      # Try convert one xlsx for a invalid format
+      (open(join('data', 'test.xlsx')).read(), 'xlsx', 'xyz'),
+    ]
+
+def test_suite():
+  return make_suite(TestServer)
