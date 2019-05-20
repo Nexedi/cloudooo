5edc710ca522de90f824db638444a5c07f477488
Boris Kocherov <bk@raskon.ru>
2019-05-20 20:37:17 +0300

    cleanup

--- cloudooo/handler/ooo/tests/testOooUnoConverter.py
+++ cloudooo/handler/ooo/tests/testOooUnoConverter.py
@@ -31,7 +31,7 @@ import magic
 import pkg_resources
 from subprocess import Popen, PIPE
 from os.path import exists, join
-from cloudooo.tests.handlerTestCase import HandlerTestCase, make_suite
+from cloudooo.tests.handlerTestCase import HandlerTestCase
 from cloudooo.handler.ooo.application.openoffice import openoffice
 from cloudooo.handler.ooo.document import FileSystemDocument
 
