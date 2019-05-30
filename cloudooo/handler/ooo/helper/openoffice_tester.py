e1b72e0eb6d3d1d6b30f98146a62630865f5c8bf
Boris Kocherov <bk@raskon.ru>
2019-05-30 17:16:50 +0300

    correctly terminate libreoffice daemon

--- cloudooo/handler/ooo/helper/openoffice_tester.py
+++ cloudooo/handler/ooo/helper/openoffice_tester.py
@@ -9,10 +9,17 @@ except ImportError:
   import simplejson as json
 
 
-def test_openoffice(connection, uno_path, office_binary_path):
+def test_openoffice(connection, uno_path, office_binary_path, terminate=False):
   import pyuno
   try:
-    helper_util.getServiceManager(connection, uno_path, office_binary_path)
+    sm = helper_util.getServiceManager(connection, uno_path, office_binary_path)
+    if terminate:
+      try:
+        sm.createInstance("com.sun.star.frame.Desktop").terminate()
+      except pyuno.getClass("com.sun.star.beans.UnknownPropertyException"):
+        pass
+      except pyuno.getClass("com.sun.star.lang.DisposedException"):
+        pass
     return True
   except pyuno.getClass("com.sun.star.connection.NoConnectException"):
     return False
@@ -22,12 +29,14 @@ def main():
   try:
     opt_list, arg_list = getopt(sys.argv[1:], "",
                                 ["connection=", "uno_path=",
-                                 "office_binary_path="])
+                                 "office_binary_path=",
+                                 "terminate"])
   except GetoptError as e:
     sys.stderr.write("%s \nUse --connection" % e)
     sys.exit(2)
 
   connection = uno_path = office_binary_path = None
+  terminate = False
   for opt, arg in opt_list:
     if opt == "--connection":
       connection = arg
@@ -37,9 +46,12 @@ def main():
         sys.path.append(uno_path)
     elif opt == "--office_binary_path":
       office_binary_path = arg
+    elif opt == "--terminate":
+      terminate = True
 
   output = json.dumps(test_openoffice(connection,
-                               uno_path, office_binary_path))
+                               uno_path, office_binary_path,
+                                      terminate=terminate))
   sys.stdout.write(output)
 
 if __name__ == "__main__":
