62a870bc3bd11a9440a33cd15fbfa943e4587ac5
Jérome Perrin <jerome@nexedi.com>
2025-05-23 13:11:46 +0900

    testOooUtil: do not check for non existant type
    This depends on python's mimetype module which might or might not
    have an enty for ogv and 3gp, we don't care about this, what this test
    covers is only that once our mimetype database is loaded, our mimetypes
    are used for these.

--- cloudooo/handler/ooo/tests/testOooUtil.py
+++ cloudooo/handler/ooo/tests/testOooUtil.py
@@ -48,10 +48,6 @@ class TestUtil(unittest.TestCase):
 
   def testLoadMimetypelist(self):
     """Test if the file with mimetypes is loaded correctly"""
-    self.assertEqual(mimetypes.types_map.get(".ogv"), None)
-    self.assertEqual(mimetypes.types_map.get(".3gp"), None)
     util.loadMimetypeList()
     self.assertEqual(mimetypes.types_map.get(".ogv"), "video/ogg")
     self.assertEqual(mimetypes.types_map.get(".3gp"), "video/3gpp")
-
-
